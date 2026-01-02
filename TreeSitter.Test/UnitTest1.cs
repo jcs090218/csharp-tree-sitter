98f7bbea777b184fe29a2b10668bdec09fb8e5b7
JenChieh <jcs090218@gmail.com>
2026-01-03 02:32:43 +0800

    test: Add basic tests

--- TreeSitter.Test/UnitTest1.cs
+++ TreeSitter.Test/UnitTest1.cs
@@ -1,15 +1,74 @@
-﻿namespace TreeSitter.Test;
+﻿using TreeSitter.Bundle;
 
-public class Tests
+namespace TreeSitter.Test
 {
-    [SetUp]
-    public void Setup()
+    public class Tests
     {
-    }
+        public static DirectoryInfo TryGetSlnDirInfo(string currentPath = null)
+        {
+            var directory = new DirectoryInfo(currentPath ?? Directory.GetCurrentDirectory());
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+            while (directory != null && !directory.GetFiles("*.sln").Any())
+                directory = directory.Parent;
+
+            return directory;
+        }
+
+        private static string FromSlnDir(params string[] paths)
+        {
+            DirectoryInfo slnDir = TryGetSlnDirInfo();
+
+            var jPaths = new List<string>();
+
+            jPaths.Add(slnDir.FullName);
+
+            jPaths.AddRange(paths);
+
+            return Path.Combine(jPaths.ToArray());
+        }
+
+        private static string FromProjectDir(params string[] paths)
+        {
+            DirectoryInfo slnDir = TryGetSlnDirInfo();
+
+            var jPaths = new List<string>();
+
+            jPaths.Add(slnDir.FullName);
+            jPaths.Add("TreeSitter.Test");
+
+            jPaths.AddRange(paths);
+
+            return Path.Combine(jPaths.ToArray());
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            TreeSitter.EnsurePrebuilt();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void parse_cpp()
+        {
+            using var parser = new TSParser();
+
+            TSLanguage lang = TreeSitterBundle.Load(TreeSitterBundle.Language.cpp);
+
+            parser.set_language(lang);
+
+            string path = FromProjectDir("fixtures", "cpp", "example_1.cpp");
+
+            var filetext = File.ReadAllText(path);
+
+            using var tree = parser.parse_string(null, filetext);
+
+            Assert.Pass();
+        }
     }
 }
---
+++ TreeSitter.Test/fixtures/cpp/example_1.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // Include the input/output stream library
+
+int main() {
+    // std::cout prints to the console
+    // std::endl adds a newline character and flushes the stream
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0; // Indicates that the program ended successfully
+}
